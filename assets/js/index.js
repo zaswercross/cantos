162df4fc97fe19e5d8dedce55f48c063866366be
Eleazar Estrada <eleazar.estrada@eficasia.com>
2021-08-05 02:07:15 -0500

    changes2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,17 @@
 var audio = new Audio();
 document.querySelectorAll('.btn-play').forEach( e => {
 	e.addEventListener('click', el => {
-		let bpm = el.target.attributes['data-bpm'].value;
-		bpm = 'assets/song/'+bpm+'.mp3';
-		audio.src = bpm;
-		audio.play();
+		setTimeout(()=>{
+			let bpm = el.target.attributes['data-bpm'].value;
+			bpm = 'assets/song/'+bpm+'.mp3';
+			audio.src = bpm;
+			audio.play();
+		},300)
 	})
 })
 document.querySelector('.btn-stop').addEventListener('click', el =>{
 	audio.pause();	
+	audio.src = '';
 })
 
 // const arNotes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'Cm', 'C#m', 'Dm', 'D#m', 'Em', 'Fm', 'F#m', 'Gm', 'G#m', 'Am', 'A#m', 'Bm'];
